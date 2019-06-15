ea8444529de8ea02ad40f66808aa3d8d2f09060f
phanvantri <36974573+phanvantri@users.noreply.github.com>
2019-06-15 10:18:39 +0700

    .

--- src/grouptwo/quizexam/data/ConnectionConfigure.java
+++ src/grouptwo/quizexam/data/ConnectionConfigure.java
@@ -1,8 +1,7 @@
 package grouptwo.quizexam.data;
 
 public final class ConnectionConfigure {
-	public final static String hostname = "localhost";
-	public final static String dbName = "onlinequiz";
-	public final static String userName = "root";
-	public final static String password = "2105";
-}
+	public final static String hostname = "den1.mysql5.gear.host";
+	public final static String dbName = "onlinequiz1";
+	public final static String userName = "onlinequiz1";
+	public final static String password = "Xf062r!?zK2D";
